1214f1752f5e19191c1d740ec85c0f15ce6d4e63
Olivier Melcher <olivier.melcher@gmail.com>
2017-07-11 00:19:39 -0500

    add default lambda and api gateway roles

--- iam-role/main.tf
+++ iam-role/main.tf
@@ -6,12 +6,128 @@ variable "environment" {
   description = "The name of the environment for this stack"
 }
 
+resource "aws_iam_role" "default_lambda_role" {
+  name = "lambda-role-${var.name}-${var.environment}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": [
+          "lambda.amazonaws.com",
+        ]
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "eni" {
+  role       = "${aws_iam_role.default_lambda_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_iam_role_policy" "default_lambda_function_role_policy" {
+  name = "lambda-function-role-policy-${var.name}-${var.environment}"
+  role = "${aws_iam_role.default_lambda_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Action": [
+        "dynamodb:BatchGetItem",
+        "dynamodb:BatchWriteItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:DescribeLimits",
+        "dynamodb:DescribeReservedCapacity",
+        "dynamodb:DescribeReservedCapacityOfferings",
+        "dynamodb:DescribeStream",
+        "dynamodb:DescribeTable",
+        "dynamodb:DescribeTimeToLive",
+        "dynamodb:GetItem",
+        "dynamodb:GetRecords",
+        "dynamodb:GetShardIterator",
+        "dynamodb:ListStreams",
+        "dynamodb:ListTables	*",
+        "dynamodb:ListTagsOfResource",
+        "dynamodb:PutItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+        "dynamodb:TagResource",
+        "dynamodb:UpdateItem",
+        "dynamodb:UpdateTimeToLive",
+        "dynamodb:UntagResource"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:dynamodb:*:*:*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "default_api_gateway_role" {
+  name = "api-gateway-role-${var.name}-${var.environment}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": ["apigateway.amazonaws.com"]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "default_apigateway_role_policy" {
+  name = "api-gateway-role-policy-${var.name}-${var.environment}"
+  role = "${aws_iam_role.default_api_gateway_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ],
+      "Action": [
+        "lambda:InvokeFunction"
+      ]
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_role" "default_ecs_role" {
   name = "ecs-role-${var.name}-${var.environment}"
 
   assume_role_policy = <<EOF
 {
-  "Version": "2008-10-17",
+  "Version": "2012-10-17",
   "Statement": [
     {
       "Action": "sts:AssumeRole",
@@ -100,11 +216,27 @@ resource "aws_iam_instance_profile" "default_ecs" {
   role  = "${aws_iam_role.default_ecs_role.name}"
 }
 
+output "default_lambda_role_id" {
+  value = "${aws_iam_role.default_lambda_role.id}"
+}
+
+output "default_lambda_role_arn" {
+  value = "${aws_iam_role.default_lambda_role.arn}"
+}
+
+output "default_api_gateway_role_id" {
+  value = "${aws_iam_role.default_api_gateway_role.id}"
+}
+
+output "default_api_gateway_role_arn" {
+  value = "${aws_iam_role.default_api_gateway_role.arn}"
+}
+
 output "default_ecs_role_id" {
   value = "${aws_iam_role.default_ecs_role.id}"
 }
 
-output "arn" {
+output "default_ecs_role_arn" {
   value = "${aws_iam_role.default_ecs_role.arn}"
 }
 
